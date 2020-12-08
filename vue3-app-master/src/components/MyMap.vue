c1c5f06b9d80cdc41e49934696b4186b55490b84
Alexis Merienne <alexis.merienne@gmail.com>
2020-12-08 14:07:22 +0100

    Ajout d'une carte intéractive

---
+++ vue3-app-master/src/components/MyMap.vue
@@ -0,0 +1,56 @@
+<template>
+  <div style="height: 75vh; width: 50vw;">
+    <l-map
+            v-model="zoom"
+            v-model:zoom="zoom"
+            :center="[43.6238, 7.0996]"
+            v-model:maxZoom="maxZoom"
+            v-model:minZoom="minZoom"
+            v-model:maxBounds="maxBounds"
+            @move="log('move')"
+            @center="log(this.getCenter())"
+    >
+      <l-tile-layer
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <l-control-layers />
+    </l-map>
+  </div>
+</template>
+
+<script>
+  import {
+    LMap,
+    LTileLayer,
+    LControlLayers,
+  } from "@vue-leaflet/vue-leaflet";
+  import "leaflet/dist/leaflet.css";
+
+
+export default {
+  components: {
+          LMap,
+          LTileLayer,
+          LControlLayers,
+  },
+  data() {
+    return {
+      zoom: 15,
+      maxZoom: 18,
+      minZoom:13,
+      maxBounds:[[43.6200,7.0950],[43.6300,7.1000]],
+      iconWidth: 25,
+      iconHeight: 40,
+    };
+  },
+  methods: {
+    log(a) {
+      console.log(a);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- vue3-app-master/src/main.js
+++ vue3-app-master/src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'leaflet/dist/leaflet.css';
+
 
 createApp(App).mount('#app')
+
