5e7c024bd62385d11e47569245da3737127ab20d
Benjamin-deLaverny <benjamin.de-laverny@etu.unice.fr>
2020-12-09 10:45:54 +0100

    Premier ajout de listes avec drag and drop pour le langage dédié. #6

--- web/src/components/MyDsl.vue
+++ web/src/components/MyDsl.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
    Hello from DSL
+    <DragDrop/>
   </div>
 </template>
 
 <script>
+
+import DragDrop from "@/components/DSL/FirstDrag&Drop";
+
 export default {
     name:"MyDsl",
+  components: {DragDrop},
 
 };
 </script>
