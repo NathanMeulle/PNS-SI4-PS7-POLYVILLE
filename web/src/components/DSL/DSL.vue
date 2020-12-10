02f5b2e55d8351734b52277b0f7acb0e49c531df
Benjamin-deLaverny <benjamin.de-laverny@etu.unice.fr>
2020-12-10 11:07:39 +0100

    Début de l'interpréteur avec message d'erreur et ajout de cases pour le DSL #6

--- web/src/components/DSL/DSL.vue
+++ web/src/components/DSL/DSL.vue
@@ -1,29 +1,38 @@
 <template>
-  <div>
-    <div class='drop-zone' @drop='onDrop($event, 1)' @dragover.prevent @dragenter.prevent>
-      Conditions :
-      <div v-for='item in listOne' :key='item.title' class='drag-el' draggable="true" @dragstart='startDrag($event, item)'>
-        {{ item.title }}
-        {{item.input}}
-        <label>
-          <input v-model="this.message[item.id]">
-          <button v-on:click="validation(item.id)">OK</button>
-        </label>
-      </div>
+  <span id="error">{{error}}</span>
+  <div class='drop-zone' @drop='onDrop($event, 5)' @dragover.prevent @dragenter.prevent id="data">
+    Données :
+    <div v-for='item in listInput' :key='item.title' class='drag-el' draggable="true" @dragstart='startDrag($event, item)'>
+      {{item.input}}
+      <label>
+        <input v-model="this.message[item.id]">
+        <button v-on:click="validation(item.id)">OK</button>
+      </label>
     </div>
-    <div class='drop-zone' @drop='onDrop($event, 3)' @dragover.prevent @dragenter.prevent>
-      Personnes :
-      <div v-for='item in listThree' :key='item.title' class='drag-el'  draggable="true" @dragstart='startDrag($event, item)'>
-        {{ item.title }}
-        {{item.input}}
-      </div>
+  </div>
+  <div class='drop-zone' @drop='onDrop($event, 1)' @dragover.prevent @dragenter.prevent id="cond">
+    Conditions :
+    <div v-for='item in listCond' :key='item.title' class='drag-el'  draggable="true" @dragstart='startDrag($event, item)'>
+      {{ item.title }}
     </div>
-    <div class='drop-zone' @drop='onDrop($event, 2)' @dragover.prevent @dragenter.prevent>
-      Mon programme :
-      <div v-for='item in listTwo' :key='item.title' class='drag-el'  draggable="true" @dragstart='startDrag($event, item)'>
-        {{ item.title }}
-        {{item.input}}
-      </div>
+  </div>
+  <div class='drop-zone' @drop='onDrop($event, 3)' @dragover.prevent @dragenter.prevent>
+    Personnes :
+    <div v-for='item in listPers' :key='item.title' class='drag-el'  draggable="true" @dragstart='startDrag($event, item)'>
+      {{ item.title }}
+    </div>
+  </div>
+  <div class='drop-zone' @drop='onDrop($event, 4)' @dragover.prevent @dragenter.prevent>
+    Quartiers :
+    <div v-for='item in listQuartiers' :key='item.title' class='drag-el'  draggable="true" @dragstart='startDrag($event, item)'>
+      {{ item.title }}
+    </div>
+  </div>
+  <div class='drop-zone' @drop='onDrop($event, 2)' @dragover.prevent @dragenter.prevent>
+    Mon programme :
+    <div v-for='item in listProg' :key='item.title' class='drag-el'  draggable="true" @dragstart='startDrag($event, item)'>
+      {{ item.title }}
+      {{item.input}}
     </div>
   </div>
   <br/>
@@ -36,12 +45,14 @@ export default {
   name: "DSL",
   data () {
     return {
+      error:'',
       items: [
         {
           id: 0,
           title: 'Pour',
           input: '',
           position: -1,
+          type:1,
           list: 1
         },
         {
@@ -49,28 +60,73 @@ export default {
           title: 'Si',
           input: '',
           position: -1,
+          type:1,
           list: 1
         },
         {
           id: 2,
           title: 'Alors',
-          input: '',
           position: -1,
+          type:1,
           list: 1
         },
         {
           id: 3,
           title: 'policiers',
-          input: '',
           position: -1,
+          type:3,
           list: 3
         },
         {
           id: 4,
           title: 'citoyens',
+          position: -1,
+          type:3,
+          list: 3
+        },
+        {
+          id: 5,
+          title: 'Zone A',
+          position: -1,
+          type:4,
+          list: 4
+        },
+        {
+          id: 6,
+          title: 'Zone B',
+          position: -1,
+          type:4,
+          list: 4
+        },
+        {
+          id: 7,
+          title: 'Input',
           input: '',
           position: -1,
-          list: 3
+          type:5,
+          list: 5
+        },
+        {
+          id: 8,
+          title: 'Input',
+          input: '',
+          position: -1,
+          type:5,
+          list: 5
+        },
+        {
+          id: 9,
+          title: 'Plus grand que',
+          position: -1,
+          type:1,
+          list: 1
+        },
+        {
+          id: 10,
+          title: 'Plus petit que',
+          position: -1,
+          type:1,
+          list: 1
         },
       ],
       message : {
@@ -81,15 +137,21 @@ export default {
     }
   },
   computed: {
-    listOne () {
+    listCond () {
       return this.sortItems(this.items.filter(item => item.list === 1))
     },
-    listTwo () {
+    listProg () {
       return this.sortItems(this.items.filter(item => item.list === 2))
     },
-    listThree () {
+    listPers () {
       return this.sortItems(this.items.filter(item => item.list === 3))
-    }
+    },
+    listQuartiers () {
+      return this.sortItems(this.items.filter(item => item.list === 4))
+    },
+    listInput () {
+      return this.sortItems(this.items.filter(item => item.list === 5))
+    },
   },
 
   methods:{
@@ -103,15 +165,17 @@ export default {
       evt.dataTransfer.setData('itemID', item.id)
     },
     onDrop (evt, list) {
+      this.error = ''
       const itemID = evt.dataTransfer.getData('itemID')
       const item = this.items.find(item => item.id === Number(itemID))
-      item.list = list
-      console.log("ajout à la liste : ",item.list)
-      if(list === 1){
-        item.position=this.listOne.length+1
+      if(item.list === 2 || list!==2){
+        this.error = "Déplacement impossible"
+        item.list = item.type
       }
-      else if (list === 2){
-        item.position=this.listTwo.length+1
+      else item.list = list
+      console.log("ajout à la liste : ",item.list)
+      if (list === 2){
+        item.position=this.listProg.length+1
       }
     },
     validation(id){
@@ -120,8 +184,8 @@ export default {
       this.message[item.id] = ""
     },
     launch(){
-      console.log("envoi du programme suivant : ",this.listTwo)
-      this.$emit("launch",this.listTwo)
+      console.log("envoi du programme suivant : ",this.listProg)
+      this.$emit("launch",this.listProg)
     }
   }
 }
@@ -138,5 +202,13 @@ export default {
   background-color: #fff;
   margin-bottom: 10px;
   padding: 5px;
+}
+
+#error{
+  color: red;
+}
+
+#cond{
+  margin-top: 10px;
 }
 </style>
--- web/src/components/DSL/Interpreteur.vue
+++ web/src/components/DSL/Interpreteur.vue
@@ -1,7 +1,7 @@
 <template>
   <DSL v-on:launch="getData($event)"/>
   <h1>Interpreteur</h1>
-  <text>{{result}}</text>
+  <span id="error">{{error}}</span>
 </template>
 
 <script>
@@ -13,33 +13,64 @@ export default {
   data(){
     return{
       programme:{},
-      result: 0,
+      posZone: {},
+      error: "",
     }
   },
 
   methods:{
     getData(listeCommandes){
+      this.error = ""
       this.programme = listeCommandes
       console.log("mon programme : ",this.programme)
-      this.useless()
+      this.checkError()
+      this.getZone()
     },
-    useless(){
-      let si = 0
-      let alors = 0
-      let pour = 0
-      this.programme.forEach((item)=>{
-        console.log(item)
-        if(item.title === "Si") si=Number(item.input)
-        if(item.title === "Alors") alors=Number(item.input)
-        if(item.title === "Pour") pour=Number(item.input)
+    checkError(){
+      let valid = this.checkIfThen()
+      if(valid === 1) this.error = 'Il est nécessaire de mettre une case "Alors" si vous avez utilisé une cas "Si" et inversement'
+      if(valid === 2) this.error = 'Il est nécessaire de mettre la case "Si" avant la case "Alors"'
+    },
+    checkIfThen(){
+      let posIf = -1
+      let posThen = -1
+      let presence = false
+      this.programme.forEach((item,index)=>{
+        if(item.title === "Si") {
+          posIf = index
+          presence = true
+        }
+        else if (item.title === "Alors"){
+          posThen = index
+          presence = true
+        }
       })
-      this.result = si + alors + pour
-      console.log("res : ",this.result)
+      if(!presence) return 0
+      if(posIf === -1 || posThen === -1) return 1
+      if (posIf > posThen) return 2
+      return 0
+    },
+    getZone(){
+      this.programme.forEach((item)=>{
+        if(item.title === "Zone A"){
+          this.posZone[0] = 43.6204
+          this.posZone[1] = 7.0719
+        }
+        else if(item.title === "Zone B"){
+          this.posZone[0] = 43.6204
+          this.posZone[1] = 7.0619
+        }
+      })
+      console.log("Zone : ",this.posZone)
     }
   }
 }
 </script>
 
 <style scoped>
+#error{
+  color: red;
+}
+
 
 </style>
