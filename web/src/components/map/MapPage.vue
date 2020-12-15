8b7173e8b42ba99d11e64f8f00e6d0766e1d3f3d
NathanMeulle <54913885+NathanMeulle@users.noreply.github.com>
2020-12-15 18:35:04 +0100

    Enregistrement Checkbox #38

--- web/src/components/map/MapPage.vue
+++ web/src/components/map/MapPage.vue
@@ -1,36 +1,46 @@
 <template>
     <div class="MapPage">
         <div class="checkbox">
-        <input type="checkbox" id="checkboxParking" value="Parking" v-model="checkedOptions">
+        <input type="checkbox" id="checkboxParking" value="Parking" v-model="loadCheckbox" v-on:input="doSelect('Parking')">
         <label for="checkboxParking">Parking</label>
 
-        <input type="checkbox" id="checkboxCommerces" value="Commerces" checked="true" v-model="checkedOptions">
+        <input type="checkbox" id="checkboxCommerces" value="Commerces" v-model="loadCheckbox" v-on:input="doSelect('Commerces')">
         <label for="checkboxAffluence">Commerces</label>
 
-        <input type="checkbox" id="checkboxAffluence" value="Affluence" v-model="checkedOptions">
+        <input type="checkbox" id="checkboxAffluence" value="Affluence" v-model="loadCheckbox" v-on:input="doSelect('Affluence')">
         <label for="checkboxAffluence">Affluence</label>
 
-        <input type="checkbox" id="checkboxPolice" value="Police" v-model="checkedOptions">
+        <input type="checkbox" id="checkboxPolice" value="Police" v-model="loadCheckbox" v-on:input="doSelect('Police')">
         <label for="checkboxPolice">Présence Policière</label>
         </div>
 
-        <MapComponent class="map" v-bind:filterOption="checkedOptions" ></MapComponent>
+        <MapComponent class="map" v-bind:filterOption="loadCheckbox" ></MapComponent>
     </div>
 </template>
 
 <script>
     import MapComponent from "./MapComponent";
+    import store from "../../store/store";
 
     export default {
         name: "MapPage",
         components: {
             MapComponent,
         },
-        data() {
-            return {
-                checkedOptions: ["Commerces"]
-            }
-        }
+        computed: {
+            loadCheckbox() {
+                console.log("loading checkbox...");
+                return this.$store.getters.getCheckBox;
+            },
+        },
+        methods:{
+            doSelect(message) {
+                store.commit( {
+                    type: "setCheckbox",
+                    CheckedBox : message,
+                })
+            },
+        },
     };
 </script>
 
