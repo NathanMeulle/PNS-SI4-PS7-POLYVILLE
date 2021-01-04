1428a41b108ceda61ab839ec36bb2bfcab24e7da
julien <julien0kaplan@gmail.com>
2021-01-04 11:46:07 +0100

    textfields #18

--- web/src/components/Event/EventPage.vue
+++ web/src/components/Event/EventPage.vue
@@ -1,11 +1,31 @@
 <template>
-  <div> Hello
+  <div> Nom du point d'intérêt où appliquer l'événement
+      <input v-model="NomPointInteret" placeholder="modifiez-moi">
+    <p>Le message est : {{ NomPointInteret }}</p>
   </div>
+  <div> Nom de l'événement
+      <input v-model="NomEvenement" placeholder="modifiez-moi">
+    <p>Le message est : {{ NomEvenement }}</p>
+  </div>
+  
+<span>Le message multiligne est :</span>
+<textarea v-model="Description" placeholder="ajoutez plusieurs lignes"></textarea>
+<br>
+
+  <p style="white-space: pre-line;">{{ Description }}</p>
+
 </template>
 
 <script>
 export default {
 name: "EventPage",
+data() {
+    return {
+        NomPointInteret: "",
+        NomEvenement:"",
+        Description:"",
+    };
+}
 }
 </script>
 
